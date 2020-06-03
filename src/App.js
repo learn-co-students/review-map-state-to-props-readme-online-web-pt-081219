0bd329c5f55ddb83854f72f2f15d5d2e5e58e69b
Shaqqour <mshaqqourm@gmail.com>
2020-06-03 14:32:31 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -28,14 +27,14 @@ class App extends Component {
             Click to change user count
           </button>
           <p>{this.props.items.length}</p>
+          <p>{this.props.users.length}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+  return { items: state.items, users: state.users }
 }
 
 export default connect(mapStateToProps)(App);
